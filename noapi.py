82bb17f4396f196ba6a07e190bb742b4dfb3745c
Maras59 <marcosespino103@gmail.com>
2025-02-16 17:10:27 -0800

    Add base files

---
+++ noapi.py
@@ -0,0 +1,3 @@
+from bs4 import BeautifulSoup
+import yaml
+BASE_URL = 'https://nyaa.si/'
---
+++ nyaapi.py
@@ -0,0 +1,6 @@
+import requests
+
+BASE_URL = 'https://nyaaapi.onrender.com/docs'
+
+
+r = requests.get('http://example.com/foo/bar')
