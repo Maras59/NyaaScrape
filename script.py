b6980dc60d0228234c63d16c1b9f308a1e66c58a
Maras59 <marcosespino103@gmail.com>
2025-02-17 01:50:31 -0800

    Implement qBittorrent API

--- script.py
+++ script.py
@@ -1,28 +1,14 @@
-from datetime import datetime
-from noapi import getshowdata
-import csv
+from utils import harvest_magnet_links, start_qBit
 import yaml
 
-with open('config.yaml') as stream:
+YAML_PATH = 'config.yaml'
+
+with open(YAML_PATH) as stream:
     try:
         conf = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         exit(exc)
 
-with open('shows.csv', 'r') as read_obj:
-    r = csv.reader(read_obj)
-    show_list = list(r)[1:]
+magnet_links = harvest_magnet_links(conf=conf)
 
-magnet_links = []
-for show in show_list:
-    latest_date = datetime.strptime(show[2], "%Y-%m-%d %H:%M")
-    threshold = conf['FULL_BATCH_THRESHOLD'] if float(show[3]) < 0.0 else float(show[3])
-
-    torrent_list = getshowdata(show[0], show[1], configs=conf)
-
-    for torrent in torrent_list:
-        if torrent['date'] > latest_date and torrent['size'] < threshold:
-            show[2] = torrent['date'].strftime("%Y-%m-%d %H:%M")
-            magnet_links.append(torrent['magnet'])
-    
-print(magnet_links)
+start_qBit(magnet_links, conf=conf)
---
+++ utils.py
@@ -0,0 +1,111 @@
+
+import csv
+import datetime
+
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+
+def getshowdata(query,subgroup=None,configs=None,keywords=None):
+    """
+    Sends a query to nyaa.si and parses the result into a list
+    query: Search query for nyaa site
+    subgroup: Subtitle group, usually within sqaure brackets
+    """
+    res = requests.get(f'{configs['BASE_URL']}q={query}&s=seeders')
+    subgroup = subgroup.lower() if subgroup else None
+    soup = BeautifulSoup(res.text, 'html.parser')
+    
+    data = []
+    table = soup.find('table', attrs={'class':configs['TABLE_CLASS']})
+    table_body = table.find('tbody')
+
+    rows = table_body.find_all('tr')
+    for row in rows:
+        tmp = {}
+        cols = row.find_all('td')
+        links = row.find_all('a')
+        cols = [ele.text.strip() for ele in cols]
+        tmp['title'] = links[2].text if 'comments' in links[1].get('class', []) else links[1].text
+        tmp['magnet'] = links[3].attrs['href'] if 'magnet' in links[3].attrs['href'] else links[4].attrs['href']
+        tmp['seeders'] = int(cols[5])
+
+        # convert from string format to datetime format
+        dtime = datetime.strptime(cols[4], "%Y-%m-%d %H:%M")
+        tmp['date'] = dtime
+
+        # Calculate filesize
+        t = cols[3].split()
+        size = float(t[0])
+        convert = t[1] == 'MiB'
+        tmp['size'] = size / 1024 if convert else size
+
+        if subgroup and subgroup not in tmp['title'].lower():
+            continue
+        if keywords and keywords.lower() not in tmp['title'].lower():
+            continue
+
+        data.append(tmp)
+
+    return data
+
+
+def harvest_magnet_links(conf):
+    with open('shows.csv', 'r') as read_obj:
+        r = csv.reader(read_obj)
+        show_list = list(r)[1:]
+
+    magnet_links = []
+    for show in show_list:
+        latest_date = datetime.strptime(show[2], "%Y-%m-%d %H:%M")
+        threshold = conf['FULL_BATCH_THRESHOLD'] if float(show[3]) < 0.0 else float(show[3])
+
+        kw = show[5] if show[5] != 'None' else None
+        torrent_list = getshowdata(show[0], show[1], configs=conf, keywords=kw)
+
+        for torrent in torrent_list:
+            if torrent['date'] > latest_date and torrent['size'] < threshold:
+                show[2] = torrent['date'].strftime("%Y-%m-%d %H:%M")
+                magnet_links.append((torrent['magnet'], show[4], torrent['title']))
+    # TODO: Update CSV with updates dates
+    return magnet_links
+
+def start_qBit(magnet_links, conf):
+    # api/v2/auth/
+    # start qBit session
+    auth = conf['credentials']
+    headers = {'Referer': conf['qBit_HOST']}
+    
+    # Perform login request
+    res = requests.post(f'{conf['qBit_HOST']}api/v2/auth/login', headers=headers, data=auth)
+    print('Login response:', res)
+
+    if res.status_code != 200:
+        exit(f'Login to qBittorrent failed. Status code: {res.status_code}, Response: {res.text}')
+
+    cookies = res.cookies
+    if 'SID' not in cookies:
+        exit('No session cookie found while logging into to qBittorrent!')
+    headers['Cookie'] = f'SID={cookies['SID']}' # Attach session ID from cookies
+
+    failed = []
+    for magnet_link in magnet_links:
+        payload = {
+            'urls': magnet_link[0],
+            'savepath': magnet_link[1]
+        }
+        res = requests.post(f'{conf["qBit_HOST"]}api/v2/torrents/add', headers=headers, data=payload)
+        if res.status_code != 200:
+            failed.append(magnet_link[2])
+
+    if failed:
+        print('Failed to add the following torrents:\n')
+        for t in failed:
+            print(f'{t}\n')
+
+    res_logout = requests.post(f'{conf["qBit_HOST"]}api/v2/auth/logout', headers=headers)
+    if res_logout.status_code != 200:
+        print(f'Failed to logout. Status code: {res_logout.status_code}, Response: {res_logout.text}')
+    else:
+        print('Success!')
+        
